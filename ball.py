a5665ca15d1ac82e286166af84ebaac2235252d4
Matt Shirlaw <mshirlaw@yahoo.com>
2013-12-06 19:10:37 +1100

    created a ball class

--- ball.py
+++ ball.py
@@ -1,88 +1,23 @@
 #!/usr/bin/python
 import pygame
 
-# Function to draw box
-def draw_ball(window,x,y):
-	black=(0,0,0)
-	pygame.draw.ellipse(window,black,[x,y,30,30],0)
-
-#initialise pygame
-pygame.init()
-
-# width and height to create tuple for screen size 
-width=640
-height=480
-size = (width,height)
-
-# background color
-white=(255,255,255)
-
-# x and y velocity
-x_speed = 0
-y_speed = 0
+class Ball():
+	"""A class to represent a ball on the screen"""
+	# Ball attributes
+	color = (0,0,0)
+	x = 0
+	y = 0
+	x_speed = 0
+	y_speed = 0
+	size = 10
 	
-# initial position
-x_coord = width/2 - 15
-y_coord = height/2 - 15
-
-# initialise a window
-window = pygame.display.set_mode( size )
-pygame.display.set_caption( 'Moving Ball' )
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# game loop, wait for user to click exit
-done=False
-while done==False:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x_speed=-3
-			elif event.key == pygame.K_RIGHT:
-				x_speed=3
-			elif event.key == pygame.K_UP:
-				y_speed=-3
-			elif event.key == pygame.K_DOWN:
-				y_speed=3
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT:
-				x_speed=0
-			elif event.key == pygame.K_RIGHT:
-				x_speed=0
-			elif event.key == pygame.K_UP:
-				y_speed=0
-			elif event.key == pygame.K_DOWN:
-				y_speed=0
+	# Draw method
+	def draw_ball(self,window):
+		pygame.draw.circle(window,self.color,[self.x,self.y],self.size)
+	
+	# Move method
+	def move_ball(self):
+		self.x = self.x + self.x_speed
+		self.y = self.y + self.y_speed
 		
-	# update x position
-	if (x_coord + x_speed) < 0:
-		x_coord=0
-	elif (x_coord + x_speed) > width-30:
-		x_coord=width-30
-	else:
-		x_coord = x_coord + x_speed
-
-	#update y position
-	if (y_coord + y_speed) < 0:
-		y_coord=0
-	elif (y_coord + y_speed) > height-30:
-		y_coord=height-30
-	else:
-		y_coord = y_coord + y_speed
-   	
-   	print "(%d,%d)" % (x_coord,y_coord)
-    	
-   	# clear screen
-   	window.fill(white)
-    	
-   	# draw player
-   	draw_ball(window,x_coord,y_coord)
-    	
-   	pygame.display.flip()
-    	
-   	clock.tick(60)
-    	
-pygame.quit()
+	
---
+++ moving_ball.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+import pygame
+from ball import Ball
+
+#initialise pygame
+pygame.init()
+
+# width and height to create tuple for screen size 
+width=640
+height=480
+size = (width,height)
+
+# background color
+white=(255,255,255)
+
+# initialise a window
+window = pygame.display.set_mode( size )
+pygame.display.set_caption( 'Moving Ball' )
+
+# create the player
+player = Ball()
+player.size=20
+player.x = width/2
+player.y = height/2
+
+# get rid of mouse cursor
+pygame.mouse.set_visible(False)
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# game loop, wait for user to click exit
+done=False
+while done==False:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				player.x_speed=-3
+			elif event.key == pygame.K_RIGHT:
+				player.x_speed=3
+			elif event.key == pygame.K_UP:
+				player.y_speed=-3
+			elif event.key == pygame.K_DOWN:
+				player.y_speed=3
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT:
+				player.x_speed=0
+			elif event.key == pygame.K_RIGHT:
+				player.x_speed=0
+			elif event.key == pygame.K_UP:
+				player.y_speed=0
+			elif event.key == pygame.K_DOWN:
+				player.y_speed=0
+		elif event.type==pygame.MOUSEMOTION:
+			pos = pygame.mouse.get_pos()
+			player.x = pos[0]
+			player.y = pos[1]
+	
+	# update x position
+	if (player.x-player.size + player.x_speed) < 0:
+		player.x=player.size
+	elif (player.x + player.x_speed) > width-player.size:
+		player.x=width-player.size
+	else:
+		player.move_ball()
+
+	#update y position
+	if (player.y-player.size + player.y_speed) < 0:
+		player.y=player.size
+	elif (player.y + player.y_speed) > height-player.size:
+		player.y=height-player.size
+	else:
+		player.move_ball()
+   	
+   	print "(%d,%d)" % (player.x,player.y)
+    	
+   	# clear screen
+   	window.fill(white)
+	
+	player.draw_ball(window)
+	
+   	pygame.display.flip()
+    	
+   	clock.tick(60)
+    	
+pygame.quit()
